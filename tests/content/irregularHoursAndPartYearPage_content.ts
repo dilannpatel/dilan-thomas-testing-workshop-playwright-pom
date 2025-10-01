0c079927cc43fcb40140669559d9bf7e2387753e
Dilan Patel <dilan.patel@solirius.com>
2025-10-01 09:23:31 +0100

    Added the irregular hours and part year page test

---
+++ tests/content/irregularHoursAndPartYearPage_content.ts
@@ -0,0 +1,11 @@
+const irregularHoursAndPartYearPage_content = {
+    caption: "Calculate holiday entitlement",
+    questionTitle: "When does the leave year start?",
+    hintText: "This is usually in the employment contract. If it is not and the job was started after 1 October 1998, the leave year will start on the 1st day of the job. If the job was started on or before 1 October 1998, the leave year will start on 1 October.",
+    dayBoxText: "Day",
+    monthBoxText: "Month",
+    yearBoxText: "Year",
+    continueButton: "Continue"
+} as const;
+
+export default irregularHoursAndPartYearPage_content
