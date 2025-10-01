dd5c323cb3bbf86201a868d04653e3556ed83cbb
Dilan Patel <dilan.patel@solirius.com>
2025-10-01 11:57:28 +0100

    last question done now, summary page

--- tests/POM.spec.ts
+++ tests/POM.spec.ts
@@ -3,6 +3,10 @@ import LandingPage from "./pages/landingPage";
 import IrregularHoursPage from "./pages/irregularHoursPage";
 import IrregularHoursAndPartYearPage from "./pages/irregularHoursAndPartYearPage";
 import HolidayEntitlementPage from "./pages/holidayEntitlementPage";
+import WorkOutHolidayPage from "./pages/workOutHolidayPage";
+import DaysWorkedPerWeekPage from "./pages/daysWorkedPerWeekPage";
+
+
 
 test(`Page object model happy path for second test`, async ({ page }): Promise<void> => {
     const landingPage: LandingPage = new LandingPage();
@@ -19,11 +23,20 @@ test(`Page object model happy path for second test`, async ({ page }): Promise<v
     await irregularHoursAndPartYearPage.answerQuestion(page);
     await irregularHoursAndPartYearPage.continueOn(page);
 
-
     const holidayEntitlementPage: HolidayEntitlementPage = new HolidayEntitlementPage();
     await holidayEntitlementPage.checkPageLoads(page);
     await holidayEntitlementPage.answerQuestion(page);
     await holidayEntitlementPage.continueOn(page);
+
+    const workOutHolidayPage: WorkOutHolidayPage = new WorkOutHolidayPage();
+    await workOutHolidayPage.checkPageLoads(page);
+    await workOutHolidayPage.answerQuestion(page);
+    await workOutHolidayPage.continueOn(page);
+
+    const daysWorkedPerWeekPage: DaysWorkedPerWeekPage = new DaysWorkedPerWeekPage();
+    await daysWorkedPerWeekPage.checkPageLoads(page);
+    await daysWorkedPerWeekPage.answerQuestion(page);
+    await daysWorkedPerWeekPage.continueOn(page);
     
 });
 
